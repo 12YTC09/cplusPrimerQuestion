e858b25cd2fc56a0e32fafea8d23ee7221f60484
12YTC09 <p0361533@o365.fcu.edu.tw>
2021-06-15 21:52:01 +0800

    CH01

---
+++ chap01/exercise1_9.cpp
@@ -0,0 +1,53 @@
+
+#include <iostream>
+
+
+
+int main(){
+
+
+    int sum = 0 ,  a = 50 ;
+    while(a<=100){
+
+         sum = sum + a;
+         a++;   
+
+    }
+
+    std::cout << "---------------------" << std::endl;
+    std::cout << sum << std::endl;
+
+
+
+
+    return 0;
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
