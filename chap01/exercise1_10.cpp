4901caa5e37ee04c87aee5baaeb99b2afccc24cd
12YTC09 <p0361533@o365.fcu.edu.tw>
2021-06-15 21:59:41 +0800

    CH01

---
+++ chap01/exercise1_10.cpp
@@ -0,0 +1,51 @@
+
+#include <iostream>
+
+
+
+int main(){
+
+
+    int b = 10;
+    while(b >= 0){
+
+        std::cout << b <<std::endl;
+        b--;
+
+
+    }
+
+
+
+
+    return 0;
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
