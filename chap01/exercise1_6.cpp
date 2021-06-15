421ac1320b46a1f5c25ef937785dd9119cbd3160
12YTC09 <p0361533@o365.fcu.edu.tw>
2021-06-15 21:31:07 +0800

    CH01

---
+++ chap01/exercise1_6.cpp
@@ -0,0 +1,33 @@
+
+
+#include <iostream>
+
+
+
+int main()
+{
+
+    int v1=5 , v2 =6;
+
+    std::cout <<  "The sum of " << v1 << std::endl;
+
+    std::cout <<  " and " << v2 << std::endl;
+    std::cout <<  "is " << v1 + v2 << std::endl;
+
+    return 0;
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
